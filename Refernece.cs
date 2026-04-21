0e26139a3491be19ed8bbf73ae0236e0aba64670
Manchikanthi Tejaswi <Manchikanthi Tejaswi@TEJASWI>
2026-04-21 11:11:19 +0530

    using reference

---
+++ Refernece.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class test
+{
+	static public void Increment (ref int x)
+	{
+		x++;
+	}
+	static public void Ref()
+	{
+		int a = 0;
+		Increment(ref a);
+		Console.WriteLine(a);
+		//Console.Clear();
+	}
+}
