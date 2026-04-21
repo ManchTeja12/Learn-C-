8149a4adf64c7a94d7cde2655408cae181c2ba21
Manchikanthi Tejaswi <Manchikanthi Tejaswi@TEJASWI>
2026-04-21 10:29:41 +0530

    return type

--- Program.cs
+++ Program.cs
@@ -2,9 +2,18 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        int Add(int a, int b)
+
         {
-            Console.WriteLine("Hello, World!");
+            return a + b;
+        }
+
+        static void Main(String[] args)
+        {
+            int a = 2;
+            int b = 5;
+            int result = a + b;
+            Console.WriteLine(result);
         }
     }
 }
